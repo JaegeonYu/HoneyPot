1493793cb78ca9ef91b3bc0d374f1ec8ff3bf9ca
jack <yjk9805@naver.com>
2024-03-11 10:22:40 +0900

    init: backend init

---
+++ back-end/src/test/java/com/exa/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
