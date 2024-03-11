34a96cdb97b26c192c7bdfcd3314c6c594719fa2
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-03-11 22:42:33 +0900

    style: How to use Tab Componenet
    - Will make Card Component

--- front-end/src/app/_components/Tab/Tab.tsx
+++ front-end/src/app/_components/Tab/Tab.tsx
@@ -4,6 +4,14 @@ import * as T from '@/_types/components/tab';
 import { vars } from '@/globalTheme.css';
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 
+/**
+ * @tabTitleList: string[]
+ * Tab의 Title로 들어갈 리스트
+ * @selectedIdx: number
+ * [선택된 Tab의 Index]
+ * @selectedIdxHandler: ([...arg]: any) => void
+ * [onClick에 들어갈 함수]
+ */
 export default function Tab({ tabTitleList, selectedIdx, selectedIdxHandler }: T.TabProps) {
   return (
     <div className={S.categoryWrapper}>
--- front-end/src/app/sungje-ui-test/page.tsx
+++ front-end/src/app/sungje-ui-test/page.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import * as Comp from '@/components';
 
 export default function SungjeUiTest() {
-  return <Comp.Tab></Comp.Tab>;
+  const [isSelectedIdx, setIsSelectedIdx] = useState(0);
+  const handleIsSelectedIdx = (idx: number) => {
+    setIsSelectedIdx(idx);
+  };
+
+  return (
+    <Comp.Tab
+      selectedIdx={isSelectedIdx}
+      selectedIdxHandler={handleIsSelectedIdx}
+      tabTitleList={['공약', '약력', '관련 영상', '의안']}
+    />
+  );
 }
