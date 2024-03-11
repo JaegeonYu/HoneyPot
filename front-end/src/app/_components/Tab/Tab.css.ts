5cacf65d62edbab2465d35b07c6764ff9efd025a
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-03-11 22:34:55 +0900

    style: Tab Component
    - Create Tab Component
    - Add type

--- front-end/src/app/_components/Tab/Tab.css.ts
+++ front-end/src/app/_components/Tab/Tab.css.ts
@@ -1,6 +1,34 @@
-import { style } from '@vanilla-extract/css';
+import { vars } from '@/globalTheme.css';
+import { createVar, style } from '@vanilla-extract/css';
 
-export const wrapper = style({
-  height: '200vh',
-  backgroundColor: 'blue',
+export const categoryWrapper = style({
+  width: '100%',
+  maxWidth: 660,
+  height: 40,
+  display: 'flex',
+  gap: 16,
+});
+
+export const oneOfCategoryWidthVar = createVar();
+export const color = createVar();
+export const fontWeight = createVar();
+export const borderBottom = createVar();
+export const category = style({
+  selectors: {
+    '&:hover': {
+      borderBottom: `1px solid ${vars.colors.service.HOVER_STROKE}`,
+      color: `${vars.colors.service.MAIN_BLACK}`,
+    },
+    '&:active, &:focus': {
+      borderBottom: `1px solid ${borderBottom}`,
+    },
+  },
+
+  width: oneOfCategoryWidthVar,
+  height: '100%',
+  padding: '10px 16px',
+  color: color,
+  fontWeight: fontWeight,
+  fontSize: 14,
+  borderBottom: `1px solid ${borderBottom}`,
 });
--- front-end/src/app/_components/Tab/Tab.tsx
+++ front-end/src/app/_components/Tab/Tab.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import * as S from './Tab.css';
+import * as T from '@/_types/components/tab';
+import { vars } from '@/globalTheme.css';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
 
-export default function Tab() {
-  return <div className={S.wrapper}>TAB</div>;
+export default function Tab({ tabTitleList, selectedIdx, selectedIdxHandler }: T.TabProps) {
+  return (
+    <div className={S.categoryWrapper}>
+      {tabTitleList.map((tabTitle, idx) => (
+        <button
+          key={idx}
+          className={S.category}
+          style={assignInlineVars({
+            [S.color]: selectedIdx === idx ? vars.colors.service.MAIN_BLACK : vars.colors.service.SUB_BLACK,
+            [S.borderBottom]: selectedIdx === idx ? vars.colors.service.MAIN_BLACK : vars.colors.service.MAIN_WHITE,
+            [S.fontWeight]: selectedIdx === idx ? '700' : '400',
+            [S.oneOfCategoryWidthVar]: `${100 / 2}%`,
+          })}
+          onClick={() => selectedIdxHandler(idx)}
+        >
+          {tabTitle}
+        </button>
+      ))}
+    </div>
+  );
 }
