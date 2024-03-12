d9fb13ec489e546e6240f44368d29bf07fa115b0
김성제 <rlatjdwp0211@naver.com>
2024-03-12 14:09:02 +0900

    build: svg files moved in src/_assets
    - And add @svgs/webpack loader
    - Add import alias

---
+++ front-end/src/app/_assets/category/index.tsx
@@ -0,0 +1,39 @@
+import Category0 from './0.svg';
+import Category1 from './1.svg';
+import Category2 from './2.svg';
+import Category3 from './3.svg';
+import Category4 from './4.svg';
+import Category5 from './5.svg';
+import Category6 from './6.svg';
+import Category7 from './7.svg';
+import Category8 from './8.svg';
+import Category9 from './9.svg';
+import Category10 from './10.svg';
+import Category11 from './11.svg';
+import Category12 from './12.svg';
+import Category13 from './13.svg';
+import Category14 from './14.svg';
+import Category15 from './15.svg';
+import Category16 from './16.svg';
+import Category17 from './17.svg';
+
+export {
+  Category0,
+  Category1,
+  Category2,
+  Category3,
+  Category4,
+  Category5,
+  Category6,
+  Category7,
+  Category8,
+  Category9,
+  Category10,
+  Category11,
+  Category12,
+  Category13,
+  Category14,
+  Category15,
+  Category16,
+  Category17,
+};
