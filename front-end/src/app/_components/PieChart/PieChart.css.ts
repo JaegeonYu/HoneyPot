741e36fa601a4d29b028cb319febb331734da69e
김성제 <rlatjdwp0211@naver.com>
2024-03-13 14:57:11 +0900

    style: Pie Chart

---
+++ front-end/src/app/_components/PieChart/PieChart.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const chartWrapper = style({
+  width: '100%',
+});
---
+++ front-end/src/app/_components/PieChart/PieChart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Chart as ChartJS, Legend, Tooltip } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { vars } from '@/globalTheme.css';
+
+ChartJS.register(Legend, Tooltip);
+
+export default function PieChart() {
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        data: [10, 20, 40],
+        backgroundColor: [
+          vars.colors.party.DEMOCRATIC_PARTY.LIGHT,
+          vars.colors.party.DEMOCRATIC_PARTY.SUB,
+          vars.colors.party.DEMOCRATIC_PARTY.MAIN,
+        ],
+      },
+    ],
+  };
+  return <Pie data={data}></Pie>;
+}
