cc31af4683e3a5dacba09ca3947ed4015022268c
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-03-18 00:28:20 +0900

    refactor: Re-export about SearchInput Component
    - type, componenet

--- front-end/src/app/_assets/icon/index.ts
+++ front-end/src/app/_assets/icon/index.ts
@@ -2,10 +2,22 @@ import ArrowBlack from './svgs/arrow-black.svg';
 import ArrowGray from './svgs/arrow-gray.svg';
 import HelpCircle from './svgs/help-circle.svg';
 import LinkTo from './svgs/link-to.svg';
+import SearchIcon from './svgs/search-icon.svg';
 import HomePageLogo from './svgs/home-page-icon.svg';
 import FaceboookLogo from './svgs/facebook-icon.svg';
 import NaverLogo from './svgs/naver-icon.svg';
 import XLogo from './svgs/x-icon.svg';
 import YoutubeLogo from './svgs/youtube-icon.svg';
 
-export { ArrowBlack, ArrowGray, HelpCircle, LinkTo, HomePageLogo, FaceboookLogo, NaverLogo, XLogo, YoutubeLogo };
+export {
+  ArrowBlack,
+  ArrowGray,
+  HelpCircle,
+  LinkTo,
+  SearchIcon,
+  HomePageLogo,
+  FaceboookLogo,
+  NaverLogo,
+  XLogo,
+  YoutubeLogo,
+};
