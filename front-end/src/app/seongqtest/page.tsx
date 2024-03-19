fef850eb1ed5d6f259c0be1869ad008078a95b1a
조성규 <jo41010@naver.com>
2024-03-19 13:27:17 +0900

    style: Poster width & height Props

--- front-end/src/app/seongqtest/page.tsx
+++ front-end/src/app/seongqtest/page.tsx
@@ -30,6 +30,7 @@ export default function SeongqTest() {
     bottom: ['김가빈', '권준구', '조성규'],
     bottomattendance: [30, 20, 10],
     mostlaw: ['김성제', '강건', '유잼건'],
+    mostcategory: [1, 3, 5],
   };
 
   const billdata = [
@@ -64,7 +65,7 @@ export default function SeongqTest() {
       partycolor: 'red',
       partyname: '국민의힘',
       represent: '김태호',
-      committee: 15,
+      committee: 6,
       billtitle: 'SSAFY내 식사메뉴 5가지로 증설하는 법률안안 입니다다라라라아',
       progressbar: ['TBD'],
       panelsectiondata: ['TBD'],
@@ -74,60 +75,29 @@ export default function SeongqTest() {
   return (
     mounted && (
       <>
-        <Comp.Tab
-          selectedIdx={isSelectedIdx}
-          selectedIdxHandler={handleIsSelectedIdx}
+        {/* <Comp.Tab
           tabTitleList={['전체 의안', '주목을 받았던 의안']}
-        />
-        <Comp.SearchInput inputId="12" onChange={() => {}} value="" placeholder="hello world"></Comp.SearchInput>
+          tabChildrenList={[<div>ffdfd</div>, <div>fdfdfd</div>, <div></div>]}
+        >
+          <Comp.SearchInput inputId="12" onChange={() => {}} value="" placeholder="hello world"></Comp.SearchInput>
+        </Comp.Tab> */}
+
         <div className={S.postWrapper}>
-          <Comp.Poster>
+          <Comp.Poster posterwidth="360px" posterheight="100%">
             <Comp.PieChart
               chartTitle="의안 추진 현황"
               legendList={[
-                { title: '이행', color: PALETTE.party.PEOPLE_POWER_PARTY[100] },
-                { title: '진행중', color: PALETTE.party.PEOPLE_POWER_PARTY[80] },
-                { title: '폐기', color: PALETTE.party.PEOPLE_POWER_PARTY[60] },
+                { title: '이행', color: PALETTE.party.국민의힘[100] },
+                { title: '진행중', color: PALETTE.party.국민의힘[80] },
+                { title: '폐기', color: PALETTE.party.국민의힘[60] },
               ]}
               datasetList={[30, 40, 70]}
               legendDisplay={false}
               UNIQUE_ID_FOR_LEGEND="bill-list-current-situation"
             ></Comp.PieChart>
           </Comp.Poster>
-          {/* 정당페이지에서는 여기 주석 해제하고 사용 */}
-          {/* 
-        <Comp.Poster>
-          <div className={S.textWrapper}>
-            <p className={S.fontTitle}>{data.party} 평균 출석률</p>
-            <p className={S.fontContent}>{data.attendance} %</p>
-          </div>
-          <div className={S.textWrapper}>
-            <p className={S.fontTitle}>출석률 상위 (전체 평균 {data.average} 건) </p>
-            <p className={S.fontContent}>
-              {data.top[0]} ({data.topattendance[0]} 건)
-            </p>
-            <p className={S.fontContent}>
-              {data.top[1]} ({data.topattendance[1]} 건)
-            </p>
-            <p className={S.fontContent}>
-              {data.top[2]} ({data.topattendance[2]} 건)
-            </p>
-          </div>
-          <div className={S.textWrapper}>
-            <p className={S.fontTitle}>출석률 하위 (전체 평균 {data.average} 건) </p>
-            <p className={S.fontContent}>
-              {data.bottom[1]} ({data.bottomattendance[0]} 건)
-            </p>
-            <p className={S.fontContent}>
-              {data.bottom[0]} ({data.bottomattendance[1]} 건)
-            </p>
-            <p className={S.fontContent}>
-              {data.bottom[2]} ({data.bottomattendance[2]} 건)
-            </p>
-          </div>
-        </Comp.Poster> */}
 
-          <Comp.Poster>
+          <Comp.Poster posterwidth="360px" posterheight="100%">
             <div className={S.textWrapper}>
               <p className={S.fontTitle}>가장 많이 발의한 분야</p>
               <p className={S.fontContent}></p>
@@ -142,15 +112,23 @@ export default function SeongqTest() {
                 padding: '0px 8px',
               }}
             >
-              {billdata.map((category, i) => (
-                <div key={`category-${i}`} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              {data.mostcategory.map((category, i) => (
+                <div
+                  key={`category-${i}`}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
                   <Category
-                    categoryId={category.committee}
+                    categoryId={category}
                     color={{ default: '#777777', hover: '#777777', focus: '#777777' }}
                     width="20px"
                     height="24px"
                   />
-                  <p className={S.fontContent}>{CATEGORY_LIST[category.committee].name}</p>
+                  <p className={S.fontContent}>{CATEGORY_LIST[category].name}</p>
                 </div>
               ))}
             </div>
