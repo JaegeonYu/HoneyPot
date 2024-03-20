1ed5d0a54647efaf2857566d73cf5a9cfa7a7218
조성규 <jo41010@naver.com>
2024-03-20 16:16:33 +0900

    style: mainpage assembly part

--- front-end/src/app/maintest/page.tsx
+++ front-end/src/app/maintest/page.tsx
@@ -2,35 +2,92 @@
 import React from 'react';
 import * as S from './style.css';
 import { ArrowBlack, ArrowMain } from '@/_assets/icon';
-
+import { Attendance, AttendanceShadow, BillListShadow, PromiseShadow } from '@/_assets/mainpageimg';
+import MyBeeImg from '../_assets/mainpageimg/imgs/whereismybee.png';
+import BeeList from '../_assets/mainpageimg/imgs/beelist.png';
+import LawDetail from '../_assets/mainpageimg/imgs/lawdetail.png';
+import LawList from '../_assets/mainpageimg/imgs/lawlist.png';
+import { Badge } from '@/_components';
 export default function MainTest() {
   return (
     <>
       <div className={S.mainWrapper}>
-        <div className={S.solganSection}>
-          <div className={S.solganWrapper}>
-            <p className={S.solganFont}>
-              국회의 약속부터 실천까지
-              <br />한 꿀통에 담다
+        <section>
+          <div className={S.solganSection}>
+            <div className={S.solganWrapper}>
+              <p className={S.solganFont}>
+                국회의 약속부터 실천까지
+                <br />한 꿀통에 담다
+              </p>
+            </div>
+            <div className={S.arrow}>
+              <button style={{ transform: 'rotate(180deg)' }}>
+                <ArrowMain></ArrowMain>
+              </button>
+            </div>
+          </div>
+          <div className={S.solganSubWrapper}>
+            <p className={S.solganSubFont}>
+              국가와 지역구를 위해 열심히 일한다는 여의도 꿀벌들,
+              <br />
+              여러분들은 얼마나 잘 알고 계신가요
+              <br />
+              이들의 약속과 실천을 여의도꿀통에서 확인해보세요
             </p>
           </div>
-          <div className={S.arrow}>
-            <button style={{ transform: 'rotate(180deg)' }}>
-              <ArrowMain></ArrowMain>
-            </button>
-          </div>
-        </div>
-        <div className={S.solganSubWrapper}>
-          <p className={S.solganSubFont}>
-            국가와 지역구를 위해 열심히 일한다는 여의도 꿀벌들,
-            <br />
-            여러분들은 얼마나 잘 알고 계신가요
-            <br />
-            이들의 약속부터 실천을 여의도꿀통에서 확인해보세요
-          </p>
-        </div>
+        </section>
 
-        <div className={S.tempWrapper}>테스트입니다</div>
+        <section>
+          <div className={S.assembleWrapper}>
+            <div className={S.assembleSub}>
+              <p className={S.assembleMainFont}>
+                공약, 출석률
+                <br /> 법안발의현황을
+                <br /> 한눈에
+              </p>
+              <div>
+                <AttendanceShadow></AttendanceShadow>
+                <AttendanceShadow></AttendanceShadow>
+              </div>
+            </div>
+            <div className={S.assembleSub}>
+              <div>
+                <BillListShadow></BillListShadow>
+              </div>
+              <p className={S.assembleFont}>
+                17개 위원회 분야별로
+                <br /> 확인할 수 있어요
+              </p>
+            </div>
+            <div>
+              <p className={S.assembleMainFont} style={{ textAlign: 'center' }}>
+                우리 지역 꿀벌은 열심히 하고 있을까요?
+              </p>
+
+              <div>
+                <img src={BeeList.src} width="100%"></img>
+              </div>
+              <button style={{ justifyContent: 'center', alignItems: 'center', width: '100%', display: 'flex' }}>
+                <div className={S.btn}>확인하기</div>
+              </button>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className={S.billWrapper}>
+            <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+              <span className={S.billFont}>21대 국회에서 발의된 법안의 수 </span>
+              <span className={S.billBoldFont}> {'\u00A0'}27,123</span>
+              <span className={S.billFont}>개</span>
+            </div>
+            <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+              <p className={S.billSubFont}>이 가운데 통과된 법안은 몇개일까요</p>
+            </div>
+            <div>
+              <img src={LawList.src} width="100%"></img>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
--- front-end/src/app/maintest/style.css.ts
+++ front-end/src/app/maintest/style.css.ts
@@ -5,7 +5,7 @@ export const mainWrapper = style({
   display: 'flex',
   flexDirection: 'column',
   // alignItems: 'center',
-  width: '120%',
+  width: '110%',
   overflow: 'hidden',
   position: 'relative',
 });
@@ -31,21 +31,33 @@ export const solganSubWrapper = style({
   display: 'flex',
   flexDirection: 'row',
   // alignItems: 'center',
-  marginTop: '8%',
-  marginBottom: '8%',
+  // marginTop: '8%',
+  // marginBottom: '8%',
   justifyContent: 'center',
+  backgroundColor: `${vars.colors.service.HOVER_BACKGROUND}`,
   width: '100%',
 });
 
-export const tempWrapper = style({
+export const assembleWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  // alignItems: 'center',
+  // height: '1230px',
+  rowGap: '23vh',
+  justifyContent: 'space-between',
+  // alignItems: 'center',
+  width: '100%',
+  // backgroundColor: `${vars.colors.service.HOVER_BACKGROUND}`,
+  padding: '15% 8vw',
+  position: 'relative',
+});
+
+export const assembleSub = style({
   display: 'flex',
   flexDirection: 'row',
-  // alignItems: 'center',
-  height: '1230px',
-  justifyContent: 'center',
-  alignItems: 'center',
   width: '100%',
-  backgroundColor: `${vars.colors.service.HOVER_BACKGROUND}`,
+  justifyContent: 'space-between',
+  alignItems: 'center',
 });
 
 const rotate = keyframes({
@@ -62,15 +74,86 @@ export const arrow = style({
   animationDuration: '2s',
   animationIterationCount: 'infinite',
 });
+/////////BIll//////////////
+export const billWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: '3vh',
+  justifyContent: 'space-between',
+  backgroundColor: `${vars.colors.service.HOVER_BACKGROUND}`,
+  // alignItems: 'center',
+  width: '100%',
+  padding: '15% 8vw',
+  position: 'relative',
+});
+
+//////////////////BTN TEMP/////////////
+export const btn = style({
+  width: '15vw',
+  height: '7vh',
+  backgroundColor: `${vars.colors.service.MAIN_COLOR_60}`,
+  alignItems: 'center',
+  justifyContent: 'center',
+  display: 'flex',
+  cursor: 'pointer',
+  fontWeight: 500,
+  fontSize: 16,
+  color: `${vars.colors.service.MAIN_WHITE}`,
+  borderRadius: 8,
+  boxShadow: `inset 3.00px 3.22px 6.22px -0.78px rgba(0, 0, 0, 0.15),
+  inset -4.50px -4.83px 6.22px -0.78px rgba(0, 0, 0, 0.3),
+  9.01px 9.65px 4.66px -2.33px rgba(0, 0, 0, 0.6)`,
+});
+
+// export const testclay = style({
+//   borderRadius: 3,
+//   boxShadow: `inset 3.00px 3.22px 6.22px -0.78px rgba(0, 0, 0, 0.15),
+//   inset -4.50px -4.83px 6.22px -0.78px rgba(0, 0, 0, 0.3),
+//   9.01px 9.65px 4.66px -2.33px rgba(0, 0, 0, 0.6)`,
+// });
+/////////////////////////////
 
 ////////////////////////////////////////FONT STYLE/////////////////
+
 export const solganFont = style({
   fontSize: 60,
-  fontWeight: 500,
+  fontWeight: 700,
   textAlign: 'center',
 });
+
 export const solganSubFont = style({
   fontSize: 28,
   fontWeight: 700,
+  textAlign: 'center',
+  marginTop: '12%',
+  marginBottom: '12%',
+  lineHeight: 1.5,
+});
+
+export const assembleMainFont = style({
+  fontSize: 36,
+  fontWeight: 700,
+  textAlign: 'left',
+});
+
+export const assembleFont = style({
+  fontSize: 26,
+  fontWeight: 500,
+  textAlign: 'left',
+});
+
+export const billFont = style({
+  fontSize: 36,
+  fontWeight: 500,
+  textAlign: 'center',
+});
+export const billBoldFont = style({
+  fontSize: 36,
+  fontWeight: 900,
+  textAlign: 'center',
+});
+export const billSubFont = style({
+  fontSize: 30,
+  fontWeight: 400,
   textAlign: 'center',
 });
