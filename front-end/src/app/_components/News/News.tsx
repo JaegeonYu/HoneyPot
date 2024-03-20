9eb5967a6b47e83972be9594dff20f82660af369
권준구 <pyjwe11@naver.com>
2024-03-20 09:36:39 +0900

    feat : 입문서 완료

--- front-end/src/app/_components/News/News.tsx
+++ front-end/src/app/_components/News/News.tsx
@@ -9,29 +9,46 @@ import { assignInlineVars } from '@vanilla-extract/dynamic';
  */
 
 export default function News() {
+  const itemsContent = [
+    {
+      id: 'item-1',
+      title: '문 대통령 신속히 추경',
+      date: '2024-03-10',
+      url: 'https://imnews.imbc.com/replay/2024/nwdesk/article/6581467_36515.html',
+    },
+  ];
+
+  function handleClick() {
+    window.location.href = 'https://imnews.imbc.com/replay/2024/nwdesk/article/6581467_36515.html';
+  }
+
   return (
     <div className={S.newsWrapper}>
       <div className={S.newsHeader}>
         <p>최근 국회 뉴스</p>
       </div>
-      <div className={S.newsContent}>
-        <div className={S.leftItems}>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
+      {itemsContent.map(item => (
+        <div className={S.newsContent}>
+          <div className={S.leftItems}>
+            <div className={S.itemContent} onClick={handleClick}>
+              {item.title}
+            </div>
+            <div className={S.itemDate}>{item.date}</div>
+            <div className={S.itemContent}>문 대통령 신속히 추경</div>
+            <div className={S.itemDate}>2024-03-10</div>
+            <div className={S.itemContent}>문 대통령 신속히 추경</div>
+            <div className={S.itemDate}>2024-03-10</div>
+          </div>
+          <div className={S.rightItems}>
+            <div className={S.itemContent}>문 대통령 신속히 추경</div>
+            <div className={S.itemDate}>2024-03-10</div>
+            <div className={S.itemContent}>문 대통령 신속히 추경</div>
+            <div className={S.itemDate}>2024-03-10</div>
+            <div className={S.itemContent}>문 대통령 신속히 추경</div>
+            <div className={S.itemDate}>2024-03-10</div>
+          </div>
         </div>
-        <div className={S.rightItems}>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
-          <div className={S.itemContent}>문 대통령 신속히 추경</div>
-          <div className={S.itemDate}>2024-03-10</div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
