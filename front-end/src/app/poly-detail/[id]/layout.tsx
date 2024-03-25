4b371f1e8e7fe4464493bfac75a9943e698601bd
권준구 <pyjwe11@naver.com>
2024-03-25 15:11:11 +0900

    fix : News 제거

--- front-end/src/app/poly-detail/[id]/layout.tsx
+++ front-end/src/app/poly-detail/[id]/layout.tsx
@@ -1,10 +1,3 @@
-// 'use client';
-
-// import React, { useState } from 'react';
-
-// export default function PolyListLayout({ children }: { children: React.ReactNode }) {
-//   return <>{children}</>;
-// }
 'use client';
 
 import React from 'react';
