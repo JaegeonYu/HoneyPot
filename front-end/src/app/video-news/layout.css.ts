fe8b8e9fe577f96956f86910808f2fa94b594982
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-03-25 09:07:03 +0900

    style: video-news page
    - Create VideoNews page

---
+++ front-end/src/app/video-news/layout.css.ts
@@ -0,0 +1,28 @@
+import { style } from '@vanilla-extract/css';
+
+export const wrapper = style({});
+
+export const gridWrapper = style({
+  width: '100%',
+  height: 'fit-content',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  alignItems: 'center',
+  justifyItems: 'center',
+  gap: 34,
+  padding: 16,
+  transition: 'padding 0.5s ease',
+
+  '@media': {
+    '(max-width: 1360px)': {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+    },
+    '(max-width: 1040px)': {
+      gridTemplateColumns: 'repeat(1, 1fr)',
+      padding: '16px 48px',
+    },
+    '(max-width: 690px)': {
+      gridTemplateColumns: 'repeat(1, 1fr)',
+    },
+  },
+});
