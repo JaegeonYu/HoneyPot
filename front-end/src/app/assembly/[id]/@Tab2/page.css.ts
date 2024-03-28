37d30ffbc82cc7aede11d0c0b2485570b9f5c703
rlatjdwp0211 <rlatjdwp0211@naver.com>
2024-03-29 00:09:44 +0900

    style: assembly detail Layout
    - Modified

--- front-end/src/app/assembly/[id]/@Tab2/page.css.ts
+++ front-end/src/app/assembly/[id]/@Tab2/page.css.ts
@@ -68,3 +68,38 @@ export const chartWrapper = style({
   width: '100%',
   height: '100%',
 });
+
+export const titleText = style({
+  lineHeight: '18px',
+  fontSize: '18px',
+  fontWeight: 700,
+  padding: '4px 0px',
+  marginBottom: '12px',
+  borderBottom: `1px solid ${vars.colors.service.STROKE_OR_BLUR}`,
+});
+
+export const mostCategoriesWrapper = style({
+  width: '100%',
+  height: 'fit-content',
+});
+
+export const mostCategoryContainer = style({
+  display: 'flex',
+  gap: '24px',
+  flexWrap: 'wrap',
+  width: '100%',
+  height: 'calc(82px * 2)',
+});
+
+export const categoryItem = style({
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  gap: '8px',
+  padding: '0px 16px 12px 16px',
+});
+
+export const categoryName = style({
+  fontSize: '14px',
+  textAlign: 'center',
+});
