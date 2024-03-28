4ef0f80849239b0d89e9c5fd28385a30f9d4b9cf
김성제 <rlatjdwp0211@naver.com>
2024-03-28 12:14:56 +0900

    feat: party detail QA
    - finish

--- front-end/src/app/party/[id]/_Subs/Charts/Charts.css.ts
+++ front-end/src/app/party/[id]/_Subs/Charts/Charts.css.ts
@@ -1,26 +1,37 @@
 import { vars } from '@/globalTheme.css';
-import { createVar, style } from '@vanilla-extract/css';
+import { createVar, globalStyle, style } from '@vanilla-extract/css';
 
-export const chartsWrapper = style({
-  width: '100%',
-  borderBottom: `1px solid ${vars.colors.service.STROKE_OR_BLUR}`,
-});
-
-export const title = style({
-  borderBottom: `1px solid ${vars.colors.service.STROKE_OR_BLUR}`,
-  textAlign: 'center',
-  padding: 10,
-});
-
-export const content = style({
+export const wrapper = style({
   display: 'flex',
   justifyContent: 'space-between',
+  gap: '12px',
   width: '100%',
-  padding: '24px',
+  padding: '24px 24px 48px 24px',
+
+  '@media': {
+    '(max-width: 972px)': {
+      overflowY: 'hidden',
+      overflowX: 'scroll',
+    },
+  },
+});
+globalStyle(`${wrapper}::-webkit-scrollbar`, {
+  display: 'none',
 });
 
-export const chartContent = style({
+export const posterWrapper = style({
+  width: '30%',
+  minWidth: 268,
+});
+
+export const chartTitle = style({
+  fontSize: 'clamp(14px, 1.4vw ,18px)',
+  fontWeight: 700,
+});
+
+export const legend = style({
   color: `${vars.colors.service.MAIN_BLACK}`,
+  fontSize: 'clamp(14px, 1.4vw ,18px)',
   fontWeight: 700,
   textAlign: 'center',
 });
@@ -31,24 +42,26 @@ export const attendanceRate = style({
 });
 
 export const attendanceRateTitle = style({
-  fontSize: 'clamp(14px, 1.5vw ,18px)',
+  fontSize: 'clamp(14px, 1.4vw ,18px)',
   fontWeight: 700,
   padding: '4px 0px',
 });
 
 export const memberName = style({
+  fontSize: 'clamp(12px, 1.2vw ,16px)',
   fontWeight: 500,
 });
+
 export const subText = style({
-  fontSize: 'clamp(12px, 1vw ,14px)',
+  fontSize: 'clamp(10px, 1vw ,14px)',
 });
 
 export const containerWidth = createVar();
 export const container = style({
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'space-between',
-  gap: 24,
+  justifyContent: 'space-evenly',
+  gap: 8,
   width: containerWidth,
   height: '100%',
   textAlign: 'center',
@@ -56,13 +69,21 @@ export const container = style({
 
 export const mostCategoryContent = style({
   display: 'flex',
-  justifyContent: 'space-between',
+  justifyContent: 'space-evenly',
   gap: 12,
   width: '100%',
 });
 
-export const mostAssembly = style({
+export const mostWrapper = style({
   display: 'flex',
   flexDirection: 'column',
   gap: 12,
+});
+
+export const skeletonLoading = style({
+  width: '30%',
+  minWidth: 268,
+  aspectRatio: '1 / 1.1',
+  borderRadius: '8px',
+  backgroundColor: vars.colors.service.SUB_WHITE,
 });
