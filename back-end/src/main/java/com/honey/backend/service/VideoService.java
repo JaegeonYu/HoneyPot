c794d87515737eae1a62447705b3b3ae3f708076
jack <yjk9805@naver.com>
2024-03-29 15:21:16 +0900

    feat: add video not found exception

--- back-end/src/main/java/com/honey/backend/service/VideoService.java
+++ back-end/src/main/java/com/honey/backend/service/VideoService.java
@@ -2,6 +2,8 @@ package com.honey.backend.service;
 
 import com.honey.backend.domain.video.Video;
 import com.honey.backend.domain.video.VideoRepository;
+import com.honey.backend.exception.BaseException;
+import com.honey.backend.exception.RegionErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,8 @@ public class VideoService {
     private final VideoRepository videoRepository;
 
     public void updateHits(Long videoId){
-        Video video = videoRepository.findById(videoId).orElseThrow();
+        Video video = videoRepository.findById(videoId).orElseThrow(() ->
+                new IllegalArgumentException("video not found");
         video.updateHits();
     }
 }
