3eda7a40f9f89d64db413cf03cc29e08fc8529ee
jack <yjk9805@naver.com>
2024-03-29 15:03:17 +0900

    feat: add video no content status

--- back-end/src/main/java/com/honey/backend/controller/VideoController.java
+++ back-end/src/main/java/com/honey/backend/controller/VideoController.java
@@ -1,11 +1,15 @@
 package com.honey.backend.controller;
 
+import com.honey.backend.domain.video.Video;
 import com.honey.backend.domain.video.VideoRepository;
 import com.honey.backend.response.video.VideoPage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +22,16 @@ public class VideoController {
     private final VideoRepository videoRepository;
 
     @GetMapping
-    public VideoPage searchVideo(@PageableDefault(size = 9, sort = "createdAt",
+    public ResponseEntity<VideoPage> searchVideo(@PageableDefault(size = 9, sort = "createdAt",
             direction = Sort.Direction.DESC) Pageable pageable, @RequestParam(required = false)String keyword){
         if(keyword==null || keyword.length() == 0 ) {
-            return new VideoPage(videoRepository.findAllWithKeywords(pageable));
+            return convertVideoPage(videoRepository.findAllWithKeywords(pageable));
         }
-        return new VideoPage(videoRepository.findAllWithKeywordsByVideoName(pageable, keyword));
+        return convertVideoPage(videoRepository.findAllWithKeywordsByVideoName(pageable, keyword));
     }
 
-
+    private ResponseEntity<VideoPage> convertVideoPage(Page<Video> videos){
+        return videos.getContent().isEmpty() ? ResponseEntity.status(HttpStatus.NO_CONTENT).build():
+                ResponseEntity.ok(new VideoPage(videos));
+    }
 }
