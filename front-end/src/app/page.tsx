dbcf9f895a0d4c3fb736727fda24ff5dffb016b0
조성규 <jo41010@naver.com>
2024-04-01 16:05:27 +0900

    fix: main page img

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -2,26 +2,22 @@
 import React, { useRef } from 'react';
 import * as S from './style.css';
 import { ArrowBlack, ArrowMain } from '@/_assets/icon';
-import MyBeeImg from '../../public/main-img/imgs/whereismybee.png';
-import BeeList from '../../public/main-img/imgs/beelist.png';
-import LawDetail from '../../public/main-img/imgs/lawdetail.png';
-import LawList from '../../public/main-img/imgs/lawlist.png';
+
 import BillPieChart from '../../public/main-img/imgs/billpiechart.png';
 import Summary from '../../public/main-img/imgs/summary.png';
 import Top3 from '../../public/main-img/imgs/top3.png';
 import IconCollection from '../../public/main-img/imgs/iconCollection.png';
-import Pie from '../../public/main-img/imgs/Pie.png';
-import Bill from '../../public/main-img/imgs/Bill.png';
-import IconCollection2 from '../../public/main-img/imgs/iconCollection2.png';
+import PieChart from '../../public/main-img/imgs/PieChart.png';
+import BillChart from '../../public/main-img/imgs/BillChart.png';
+import AssemblyList from '../../public/main-img/imgs/assemblyList.png';
+import BillList from '../../public/main-img/imgs/billList.png';
 
 import { motion } from 'framer-motion';
-import Test from '@/_components/LoadingTest/test';
-import TextTest from '@/_components/LoadingTest/TextTest/texttest';
-import TextSpinnerLoader from '@/_components/LoadingTest/TextSpinner/TextSpinner';
-import DotLoading from '@/_components/Bill/Subs/DotLoading/DotLoading';
+
 import Slogan from './Slogan';
 import Image from 'next/image';
 import Link from 'next/link';
+import TextTest from './_components/LoadingTest/TextTest/texttest';
 
 export default function MainTest() {
   const scrollToRef = useRef<HTMLDivElement>(null);
@@ -36,17 +32,12 @@ export default function MainTest() {
     <>
       <div className={S.mainWrapper}>
         <section>
-          {/* <div className={S.test} id="1">
-            <TextTest contents=""></TextTest>
-          </div> */}
-
           <div className={S.solganSection}>
             <div className={S.solganWrapper}>
-              {/* <p className={S.solganFont}>
-                국회의 약속부터 실천까지
-                <br />한 꿀통에 담다
-              </p> */}
               <Slogan></Slogan>
+            </div>
+            <div className={S.test} id="1">
+              <TextTest contents=""></TextTest>
             </div>
 
             <div className={S.arrow} onClick={scrollToTop}>
@@ -60,7 +51,7 @@ export default function MainTest() {
             whileInView={{
               opacity: 1,
               y: 0,
-              transition: { delay: 0.7 },
+              transition: { delay: 0.3 },
             }}
           >
             <div className={S.solganSubWrapper} ref={scrollToRef}>
@@ -82,7 +73,7 @@ export default function MainTest() {
               whileInView={{
                 opacity: 1,
                 y: 0,
-                transition: { delay: 0.7 },
+                transition: { delay: 0.3 },
               }}
             >
               <div className={S.assembleSub}>
@@ -91,19 +82,10 @@ export default function MainTest() {
                   <br /> 법안발의현황을
                   <br /> 한눈에
                 </p>
-                {/* <div style={{ display: 'flex', flexDirection: 'row', width: '70%' }}>
-                  
 
-                  <motion.div whileHover={{ scale: 1.1 }}>
-                    <img src={Pie.src} width="90%" className={S.shadow1}></img>
-                  </motion.div>
-                  <motion.div whileHover={{ scale: 1.1 }}>
-                    <img src={Bill.src} width="90%" className={S.shadow1}></img>
-                  </motion.div>
-                </div> */}
-                <div>
-                  <Image width={300} height={300} alt="차트이미지" src={'/main-img/svgs/attendanceShadow.svg'} />
-                  <Image width={300} height={300} alt="차트이미지" src={'/main-img/svgs/attendanceShadow.svg'} />
+                <div style={{ width: '60%', display: 'flex', flexDirection: 'row', gap: '20px' }}>
+                  <img className={S.shadow1} src={BillChart.src} width={'50%'}></img>
+                  <img className={S.shadow1} src={PieChart.src} width={'50%'}></img>
                 </div>
               </div>
             </motion.div>
@@ -112,7 +94,7 @@ export default function MainTest() {
               whileInView={{
                 opacity: 1,
                 y: 0,
-                transition: { delay: 0.7 },
+                transition: { delay: 0.3 },
               }}
             >
               <div className={S.assembleSub}>
@@ -134,7 +116,7 @@ export default function MainTest() {
                 whileInView={{
                   opacity: 1,
                   y: 0,
-                  transition: { delay: 0.7 },
+                  transition: { delay: 0.3 },
                 }}
               >
                 <p className={S.assembleMainFont} style={{ textAlign: 'center' }}>
@@ -142,7 +124,7 @@ export default function MainTest() {
                 </p>
 
                 <div>
-                  <img src={BeeList.src} width="100%"></img>
+                  <img src={AssemblyList.src} width="100%"></img>
                 </div>
                 <Link href={`assemblies/`}>
                   <button style={{ justifyContent: 'center', alignItems: 'center', width: '100%', display: 'flex' }}>
@@ -160,12 +142,12 @@ export default function MainTest() {
               whileInView={{
                 opacity: 1,
                 y: 0,
-                transition: { delay: 0.7 },
+                transition: { delay: 0.3 },
               }}
             >
               <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                 <span className={S.assembleMainFont}>21대 국회에서 발의된 법안의 수 </span>
-                <span className={S.billBoldFont}> {'\u00A0'}27,123</span>
+                <span className={S.billBoldFont}> {'\u00A0'}23,604</span>
                 <span className={S.billBoldFont}>개</span>
               </div>
               <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', marginBottom: 160 }}>
@@ -178,7 +160,7 @@ export default function MainTest() {
               whileInView={{
                 opacity: 1,
                 y: 0,
-                transition: { delay: 0.7 },
+                transition: { delay: 0.3 },
               }}
             >
               <div className={S.assembleSub} style={{ margin: '35% 0px' }}>
@@ -201,7 +183,7 @@ export default function MainTest() {
               whileInView={{
                 opacity: 1,
                 y: 0,
-                transition: { delay: 0.7 },
+                transition: { delay: 0.3 },
               }}
             >
               <div className={S.assembleSub} style={{ marginTop: 50 }}>
@@ -220,6 +202,38 @@ export default function MainTest() {
                 </div>
               </div>
             </motion.div>
+
+            <div className={S.assembleSub} style={{ marginTop: '40%', justifyContent: 'center' }}>
+              <motion.div
+                initial={{ opacity: 0, y: 60 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: { delay: 0.3 },
+                }}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    rowGap: '40px',
+                  }}
+                >
+                  <p className={S.assembleMainFont} style={{ textAlign: 'center' }}>
+                    여의도 꿀통과 함께 법안들을 확인해보세요
+                  </p>
+
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <img src={BillList.src} width="80%"></img>
+                  </div>
+                  <Link href={`bills/`}>
+                    <button style={{ justifyContent: 'center', alignItems: 'center', width: '100%', display: 'flex' }}>
+                      <div className={S.btn}>확인하기</div>
+                    </button>
+                  </Link>
+                </div>
+              </motion.div>
+            </div>
 
             {/* <div>
               <img className={S.testclay2} src={LawList.src} width="50%"></img>
