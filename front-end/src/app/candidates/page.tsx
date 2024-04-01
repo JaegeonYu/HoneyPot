d1a25b90e4f5d544186c07da44c0d5accb4ec2eb
김성제 <rlatjdwp0211@naver.com>
2024-04-01 17:55:21 +0900

    feat: candidates page

--- front-end/src/app/candidates/page.tsx
+++ front-end/src/app/candidates/page.tsx
@@ -1,9 +1,23 @@
-import { useQueries } from '@tanstack/react-query';
+'use client';
+
 import React from 'react';
+import * as S from './page.css';
+import * as API from '@/apis';
+import { useQueries, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 export default function CandidatesPage() {
-  //   const [{}] = useQueries({
-  //     queries: []
+  // const {
+  //   data: candidateList,
+  //   isFetched: candidateListFetched,
+  //   fetchNextPage: fetchNextCandidateList,
+  // } = useSuspenseInfiniteQuery({
+  //   queryKey: [`test`],
+  //   queryFn: ({ pageParam }) =>
+  //     API.cantidate.getCandidateList({ page: pageParam, limit: 10, sido: '', sigungu: '', dong: '' }),
+  //   initialPageParam: 0,
+  //   getNextPageParam: () => {
+  //     return null;
+  //   },
   // });
-  return <></>;
+  return <div>PAGEPAGEPAGEPAGEPAGEPAGEPAGEPAGEPAGE</div>;
 }
