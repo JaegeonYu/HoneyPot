11749c12f4a54c272918b99328fa326b9ce40ef4
조성규 <jo41010@naver.com>
2024-04-03 10:22:18 +0900

    fix: Map Nesting Bug

--- front-end/src/app/_components/KakaoMap/KakaoMap.tsx
+++ front-end/src/app/_components/KakaoMap/KakaoMap.tsx
@@ -17,27 +17,38 @@ declare global {
 function KakaoMap({ pollList }: { pollList: { name: string; address: string }[] }) {
   const mapRef = useRef<HTMLDivElement>(null);
   const searchParams = useSearchParams();
+  const [flag, setFlag] = useState<boolean>(false);
+  const [map, setMap] = useState<any>(null);
+
   // console.log(pollList, 'poLLLIST');
 
   // var dongvalue= searchParams.get('dong')
 
   useEffect(() => {
+    const script = window.kakao.maps.load(() => {
+      var mapContainer = document.getElementById('map'); // 지도를 표시할 div
+      let mapOption = {
+        center: new window.kakao.maps.LatLng(37.531804, 126.913121), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+
+      // 지도를 생성합니다
+      let map = new window.kakao.maps.Map(mapContainer, mapOption);
+      setMap(map);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (map === null) return;
     if (window.kakao) {
       const script = window.kakao.maps.load(() => {
-        var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-          mapOption = {
-            center: new window.kakao.maps.LatLng(37.531804, 126.913121), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨
-          };
-
-        // 지도를 생성합니다
-        var map = new window.kakao.maps.Map(mapContainer, mapOption);
-
         // 주소-좌표 변환 객체를 생성합니다
         var geocoder = new window.kakao.maps.services.Geocoder();
         // console.log(pollList, 'pl');
         // 주소로 좌표를 검색합니다
         var bounds = new window.kakao.maps.LatLngBounds(); //추가한 코드
+
+        // console.log(map, '이거 몇번해 ?', flag);
 
         pollList.forEach(function (position) {
           //추가한 코드
@@ -87,7 +98,7 @@ function KakaoMap({ pollList }: { pollList: { name: string; address: string }[] 
         });
       });
     }
-  }, [pollList]);
+  }, [pollList, map]);
 
   return (
     <>
