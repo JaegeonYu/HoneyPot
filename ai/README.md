3501690e22a639208f31178ace8fcde765e142bc
kanggeonnim <gun4329@gmail.com>
2024-04-16 10:32:31 +0900

    remove ai/

--- ai/README.md
+++
@@ -1,5 +0,0 @@
-## HoneyPot Project 
-주제별 영상 편집
-pytube
-whisper
-chatgpt3.5
--- ai/app/apis/api_base.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from app.apis.routes import gpt
-
-api_router = APIRouter()
-api_router.include_router(gpt.router, prefix="/gpt", tags=["gpt"])
